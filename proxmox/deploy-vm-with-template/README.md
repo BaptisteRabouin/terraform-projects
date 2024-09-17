a29da479a494224896196742883cc622eb6da839
BaptisteRabouin <nollor@gitlab.com>
2024-09-17 20:38:22 +0000

    add project

---
+++ proxmox/deploy-vm-with-template/README.md
@@ -0,0 +1,8 @@
+## VM Deployment from Preconfigured Template on Proxmox
+
+**Description**: Deployment of a VM from an existing template on Proxmox. The template is based on Ubuntu 22.04.
+
+- **Version**: Proxmox 8.2.4
+- **Template**: Ubuntu 22.04
+
+This process allows for quick VM deployment using a preconfigured template, streamlining the setup. The goal is to efficiently deploy a VM based on a ready-to-use template.
---
+++ proxmox/deploy-vm-with-template/vm.tf
@@ -0,0 +1,73 @@
+resource "proxmox_virtual_environment_vm" "terraform-vm" {
+  name            = "terraform-vm"
+  node_name       = "pve1"
+  vm_id           = 2024
+  bios            = "ovmf"
+  stop_on_destroy = true
+
+  agent {
+    enabled = true
+  }
+
+  initialization {
+    datastore_id = "local-zfs"
+    interface = "ide2"
+
+    ip_config {
+      ipv4 {
+        address = "192.168.1.160/24"
+        gateway = "192.168.1.254"
+      }
+    }
+
+    dns {
+      servers = ["1.1.1.1", "8.8.8.8"]
+    }
+
+    user_account {
+      username = "ubuntu"
+      password = "ubuntu"
+    }
+  }
+
+  clone {
+    datastore_id = "local-zfs"
+    full         = true
+    vm_id        = "9000"
+  }
+
+
+  cpu {
+    cores = 2
+    numa  = true
+    limit = 64
+    # affinity = "0-1"
+  }
+
+  memory {
+    dedicated = 2048
+    # hugepages = "2"
+  }
+
+  efi_disk {
+    datastore_id = "local-zfs"
+    file_format  = "raw"
+    type         = "4m"
+  }
+
+  disk {
+    datastore_id = "local-zfs"
+    file_format  = "raw"
+    discard      = "on"
+    interface    = "scsi0"
+    size         = 20
+  }
+
+
+  # Network config
+  network_device {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+}
